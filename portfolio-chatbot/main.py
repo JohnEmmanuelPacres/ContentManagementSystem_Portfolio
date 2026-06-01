2ac98f06d04b1f5a55f2f9459daa55f3c4c5f457
JE Pacres <pacresjohn029@gmail.com>
2026-06-02 00:04:58 +0800

    update: Setup google genai

--- portfolio-chatbot/main.py
+++ portfolio-chatbot/main.py
@@ -2,6 +2,8 @@ import os
 # pyrefly: ignore [missing-import]
 from google import genai
 # pyrefly: ignore [missing-import]
+from google.genai import types
+# pyrefly: ignore [missing-import]
 from dotenv import load_dotenv
 # pyrefly: ignore [missing-import]
 from pydantic import BaseModel
@@ -10,14 +12,17 @@ from fastapi import FastAPI, HTTPException
 # pyrefly: ignore [missing-import]
 from fastapi.middleware.cors import CORSMiddleware
 
+
 # load environment variables
-
 load_dotenv()
-
 api_key = os.getenv("GEMINI_API_KEY")
 
-# initialize the gemini
+# initialize the gemini api
 client = genai.Client(api_key=api_key)
+
+# load the brain (portfolio_brain)
+with open("CONTEXT.md", "r", encoding="utf-8") as file:
+    portfolio_brain = file.read()
 
 # setup fastapi and cors
 app = FastAPI()
@@ -35,13 +40,18 @@ app.add_middleware(
 class ChatRequest(BaseModel):
     message: str
 
-@app.post('/chat')
+@app.post('/api/chat')
 async def handle_chat(request: ChatRequest):
     try:
         response = client.models.generate_content(
-            model='gemini-3.5-flash',
+            model='gemini-2.5-flash',
             contents=request.message,
+            config=types.GenerateContentConfig(
+                system_instruction=portfolio_brain,
+                temperature=0.3,
+            )
         )
-        return {'response': response.text}
+
+        return {'reply': response.text}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
