c5847782cd6b06aa3eff9c790221e5f9526c4b92
JE Pacres <pacresjohn029@gmail.com>
2026-06-03 01:28:39 +0800

    add: Added education schematype

---
+++ portfolio-cms/schemaTypes/education.ts
@@ -0,0 +1,32 @@
+export default{
+    name: 'education',
+    title: 'Education',
+    type: 'document',
+    fields: [
+        {
+            name: 'university',
+            title: 'University',
+            type: 'string',
+        },
+        {
+            name: 'course-name',
+            title: 'Course Name',
+            type: 'string',
+        },
+        {
+            name: 'address',
+            title: 'Address',
+            type: 'string',
+        },
+        {
+            name: 'start-year',
+            title: 'Start Year',
+            type: 'number',
+        },
+        {
+            name: 'end-year',
+            title: 'End Year',
+            type: 'number',
+        },
+    ],
+}
