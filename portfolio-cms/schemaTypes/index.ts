4ad819b295c075adb00adf5e6c2c0fdf8c1cfbdd
JE Pacres <pacresjohn029@gmail.com>
2026-06-03 01:43:26 +0800

    add: Added work schematype

--- portfolio-cms/schemaTypes/index.ts
+++ portfolio-cms/schemaTypes/index.ts
@@ -2,10 +2,12 @@ import achievement from "./achievement";
 import project from "./project";
 import education from "./education";
 import organization from "./organization";
+import work from "./work";
 
 export const schemaTypes = [
     project,
     achievement,
     education,
     organization,
+    work
 ]
